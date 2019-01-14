4de61f240ed7d97750ff6697665db365de63b270
Galen <galenhwang@yahoo.ca>
2019-01-13 20:19:41 -0800

    Code for boom problem refactored

--- 2011-codeforces-saratov-school-regional/g-boom.cpp
+++ 2011-codeforces-saratov-school-regional/g-boom.cpp
@@ -7,65 +7,72 @@
 
 using namespace std;
 
-#define MAXN 110
+#define MAX_N 100
 
-int used[MAXN][MAXN];
-char str[MAXN][30];
+int used[MAX_N][MAX_N];
+string str[MAX_N];
 
-int a1[MAXN],b1[MAXN], a2[MAXN], b2[MAXN];
-int c[MAXN];
+int a1[MAX_N],b1[MAX_N], a2[MAX_N], b2[MAX_N];
+int c[MAX_N];
 
 int q[600000];
-int solved[MAXN];
-int ans[MAXN];
+int solved[MAX_N];
+int ans[MAX_N];
 
-vector<string> vec[MAXN];
+vector<string> vec[MAX_N];
 
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 	int n, t;
 	cin >> n >> t;
-	for(int i = 0; i < n; ++i)
+	for (int i = 0; i < n; i++)
 		cin >> a1[i] >> b1[i] >> a2[i] >> b2[i];
 	int m;
 	cin >> m;
-	for(int i = 0; i < m; ++i) scanf("%s%d", str[i], c + i);
-	int p = 0, l = 0, r = 0, rem = m, trem = t;
-	for(int i = 0; i < m; ++i) q[r++] = i;
-	while(rem) {
+	for (int i = 0; i < m; i++)
+		cin >> str[i] >> c[i];
+	int p = 0, l = 0, r = m, rem = m, t_rem = t;
+	for (int i = 0; i < m; i++)
+		q[i] = i;
+	while (rem) {
 		int pro = q[l];
-		int per = p, sum;
-		if(per < n) sum = a1[per] + b2[per];
-		else sum = a2[per - n] + b1[per - n];
+		int per = p;
+		int sum;
+		if (per < n)
+			sum = a1[per] + b2[per];
+		else
+			sum = a2[per % n] + b1[per % n];
 		int need = max(1, c[pro] - sum - used[per % n][pro]);
-		if(need <= trem) {
+		if (need <= t_rem) {
 			solved[pro] = per % n;
-			string temp(str[pro]);
-			vec[per % n].push_back(temp);
+			vec[per % n].push_back(str[pro]);
 			l++;
-			trem -= need;
-			--rem;
-			++ans[per % n];
-			if(trem == 0) {
-				++p;
-				if(p == 2 * n) p = 0;
-				trem = t;
+			t_rem -= need;
+			rem--;
+			ans[per % n]++;
+			if(t_rem == 0) {
+				p++;
+				if(p == 2 * n)
+					p = 0;
+				t_rem = t;
 			}
 		}
 		else {
-			used[per % n][pro] += trem;
+			used[per % n][pro] += t_rem;
 			l++;
 			q[r++] = pro;
-			++p;
-			if(p == 2 * n) p = 0;
-			trem = t;
+			p++;
+			if (p == 2 * n)
+				p = 0;
+			t_rem = t;
 		}
 	}
-	for(int i = 0; i < n; ++i) {
-		printf("%d", ans[i]);
-		for(int j = 0; j < vec[i].size(); ++j) cout << " " << vec[i][j];
-		puts("");
+	for (int i = 0; i < n; i++) {
+		cout << ans[i];
+		for (int j = 0; j < vec[i].size(); j++)
+		    cout << " " << vec[i][j];
+		cout << endl;
 	}
 	return 0;
 }
